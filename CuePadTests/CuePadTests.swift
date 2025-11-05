f54e592cff33207cc8a5b624f37061a88ed4cb2b
reekystive <reekystive@gmail.com>
2025-11-05 21:26:57 +0800

    Reformat code

--- CuePadTests/CuePadTests.swift
+++ CuePadTests/CuePadTests.swift
@@ -5,13 +5,11 @@
 //  Created by ReekyStive on 2025-11-05.
 //
 
-import Testing
 @testable import CuePad
+import Testing
 
 struct CuePadTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
 }
